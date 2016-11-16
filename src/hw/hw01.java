cc04c887f5487c53dcc1a66e03e6e7fed3ea8bf2
yagiliao <105021039@live.asia.edu.tw>
2016-11-16 08:35:48 +0800

    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,29 @@
 package hw;
-
+import java.util.*;
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int x=scn.nextInt();
+		int y=scn.nextInt();
+		int z=scn.nextInt();
+		System.out.println(lcm(x,y,z));
+	}
+	public static int lcm(int x,int y,int z){
+		int max1=1,max2=1,sum;
+		for(int i=1;i<=x;i++){
+			if(x%i==0 && y%i==0){
+				max1=i;
+			}
+		}
+		for(int i=1;i<=max1;i++){
+			if(max1%i==0 && z%i==0){
+				max2=i;
+			}
+		}
+		sum=(x/max2)*(y/max2)*(z/max2)*max2;
+		return sum;
 	}
 
 }
--- src/hw/hw04.java
+++ src/hw/hw04.java
@@ -1,10 +1,22 @@
 package hw;
-
+import java.util.*;
 public class hw04 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn=new Scanner(System.in);
+		int n=scn.nextInt();
+		int m=scn.nextInt();
+		System.out.println(gcd(n,m));
+	}
+	public static int gcd(int n,int m){
+		int max=1;
+		for(int i=1;i<=n;i++){
+			if(n%i==0 && m%i==0){
+				max=i;
+			}
+		}
+		return max;
 	}
 
 }
